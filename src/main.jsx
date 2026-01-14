debf90f4a1ffdb1b4d4199015d49f9cec3a809a4
Petko Todorov <p.todorov1015@gmail.com>
2026-01-14 13:49:18 +0200

    Wrap App in ThemeWrapper

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import { ThemeWrapper } from './providers/ThemeWrapper';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <App />
+        <ThemeWrapper>
+            <App />
+        </ThemeWrapper>
     </StrictMode>
 );
