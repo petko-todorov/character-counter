8e74e2904e0559f99968ece5a660a7f7c7b719dc
Petko Todorov <p.todorov1015@gmail.com>
2026-01-14 15:27:47 +0200

    Add header and main article text.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import { ThemeToggle } from './components/ThemeToggle';
+import Header from './components/Header';
 
 function App() {
     return (
         <>
-            <ThemeToggle />
+            <div className="px-[10%] ">
+                <Header />
+
+                <article className="text-4xl text-center font-semibold">
+                    Analyze your text in real-time.
+                </article>
+            </div>
         </>
     );
 }
