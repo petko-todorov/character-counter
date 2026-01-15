dd5c2fbe98506826862603b5af673d6ee450b8f2
Petko Todorov <p.todorov1015@gmail.com>
2026-01-15 13:50:22 +0200

    Add entered text and total characters slices to store
    Integrated createEnteredTextSlice and createTotalCharactersSlice into the Zustand store to manage entered text and total character count state.

--- src/store/useStore.js
+++ src/store/useStore.js
@@ -1,6 +1,10 @@
 import { create } from 'zustand';
 import { createThemeSlice } from './slices/createThemeSlice';
+import { createdEnteredTextSlice } from './slices/createEnteredTextSlice';
+import { createTotalCharactersSlice } from './slices/createTotalCharactersSlice';
 
 export const useStore = create((set) => ({
     ...createThemeSlice(set),
+    ...createdEnteredTextSlice(set),
+    ...createTotalCharactersSlice(set),
 }));
