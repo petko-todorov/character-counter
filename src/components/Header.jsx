f40983f6ac1e9e3e1cbcb89674d395fa34e47f56
Petko Todorov <p.todorov1015@gmail.com>
2026-01-17 14:05:55 +0200

    Adjust header padding for better spacing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { ThemeToggle } from '../components/ThemeToggle';
 const Header = () => {
     return (
         <>
-            <header className="flex justify-between items-center py-10">
+            <header className="flex justify-between items-center pt-10 pb-5">
                 <h1 className="text-2xl">Character counter</h1>
 
                 <ThemeToggle />
