86032af4a7719b18dee202596a314bd9defb67da
Petko Todorov <p.todorov1015@gmail.com>
2026-01-17 14:07:01 +0200

    Add include spaces handler slice to store

--- src/store/useStore.js
+++ src/store/useStore.js
@@ -2,9 +2,11 @@ import { create } from 'zustand';
 import { createThemeSlice } from './slices/createThemeSlice';
 import { createdEnteredTextSlice } from './slices/createEnteredTextSlice';
 import { createTotalCharactersSlice } from './slices/createTotalCharactersSlice';
+import { createIncludeSpacesHandlerSlice } from './slices/createIncludeSpacesHandlerSlice';
 
 export const useStore = create((set) => ({
     ...createThemeSlice(set),
     ...createdEnteredTextSlice(set),
     ...createTotalCharactersSlice(set),
+    ...createIncludeSpacesHandlerSlice(set),
 }));
