811a407cec539be7694afc8a2d6f929bda940475
Petko Todorov <p.todorov1015@gmail.com>
2026-01-18 19:04:21 +0200

    Update IncludeSpaces component to use Radix UI checkbox

--- src/components/IncludeSpaces.jsx
+++ src/components/IncludeSpaces.jsx
@@ -1,8 +1,29 @@
+import { Checkbox, Flex, Text } from '@radix-ui/themes';
+import { useStore } from '../store/useStore';
+
 const IncludeSpaces = () => {
+    const includeSpaces = useStore((state) => state.includeSpaces);
+    const toggleIncludeSpaces = useStore((state) => state.toggleIncludeSpaces);
+
     return (
         <>
-            <input type="checkbox" id="include-spaces" />
-            <label htmlFor="include-spaces">Include spaces</label>
+            {/* <input
+                type="checkbox"
+                id="include-spaces"
+                checked={includeSpaces}
+                onChange={(e) => toggleIncludeSpaces(e.target.checked)}
+            />
+            <label htmlFor="include-spaces">Include spaces</label> */}
+            <Text as="label" size="3">
+                <Flex gap="2">
+                    <Checkbox
+                        checked={includeSpaces}
+                        onCheckedChange={toggleIncludeSpaces}
+                        size="3"
+                    />
+                    Include spaces
+                </Flex>
+            </Text>
         </>
     );
 };
